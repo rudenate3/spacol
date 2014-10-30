042a3095203620c13f097f13f2660b95103f4345
rudenate3 <nathan.odin+github@gmail.com>
2014-10-30 11:43:37 -0400

    Test 2

--- src/spacol/Main.java
+++ src/spacol/Main.java
@@ -1,8 +1,10 @@
+package spacol;
+
 /**
  * Created by hdtech on 10/30/2014.
  */
 public class Main {
-    public Main() {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
